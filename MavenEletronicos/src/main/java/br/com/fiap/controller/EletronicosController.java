3a7601e0f632a707f2edea2e5a8dded3669117f9
Lucas Gabriel <lg.carlos@ymail.com>
2020-04-27 22:38:58 -0300

    Bug da Lista

--- MavenEletronicos/src/main/java/br/com/fiap/controller/EletronicosController.java
+++ MavenEletronicos/src/main/java/br/com/fiap/controller/EletronicosController.java
@@ -1,15 +1,29 @@
 package br.com.fiap.controller;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import br.com.fiap.model.EletronicosModel;
+
 @Controller
 public class EletronicosController {
 	
 	@RequestMapping (value= {"/eletronicos", "/"}, method = RequestMethod.GET)
-	public String eletronicos(Model model) {
-		model.addAttribute("teste", "Teste do Controller");
-		return "teste";
+	public String getEletronicos(Model model) {
+	
+		ArrayList<EletronicosModel> listaEletronicos = new ArrayList<>();
+		listaEletronicos.add(new EletronicosModel("Celular 1", "Celular"));
+		listaEletronicos.add(new EletronicosModel("Televisão 1", "Televisão"));
+		listaEletronicos.add(new EletronicosModel("Mémoria Ram 1", "Hardware"));
+		listaEletronicos.add(new EletronicosModel("Fonte de Alimentação 1", "Hardware"));
+		listaEletronicos.add(new EletronicosModel("Notebook 1", "Notebook"));
+		listaEletronicos.add(new EletronicosModel("Desktop 1", "Desktop"));
+		listaEletronicos.add(new EletronicosModel("Carregador 1", "Acessórios"));
+		listaEletronicos.add(new EletronicosModel("HD 1", "Hardware"));
+		model.addAttribute("listaEletronicos", listaEletronicos);
+		return "eletronicos";
 	}
 }
--- MavenEletronicos/src/main/webapp/pages/eletronicos.jsp
+++ MavenEletronicos/src/main/webapp/pages/eletronicos.jsp
@@ -1,12 +1,38 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="UTF-8"%>
+	pageEncoding="ISO-8859-1"%>
+
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
+
+<title>Eletronicos</title>
+
 </head>
+
 <body>
-	<h1>Teste</h1>
+
+	<h1>Eletronicos</h1>
+	<div class="lista">
+		<table>
+			<tr>
+				<th>Nome</th>
+				<th>Tipo</th>
+			</tr>
+		<c:forEach items="${listaEletronicos}" var="itemList">
+			<tr>
+				<td>${itemList.nome}</td>
+				<td>${itemList.getNome()}</td>
+			</tr>
+		</c:forEach>
+		</table>
+	</div>
+
 </body>
 </html>
